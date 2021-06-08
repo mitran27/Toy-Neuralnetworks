d053337115ebb5710de3919d3286494400442a6c
Mitran R <62206653+mitran27@users.noreply.github.com>
2021-06-08 13:32:46 +0530

    Update Ann.cpp

--- Ann.cpp
+++ Ann.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+# include <assert.h>
+
 using namespace std;
 
 class Matrix{
@@ -49,28 +51,9 @@ class Matrix{
                 }
             }
     }
-    Matrix operator*(Matrix mat2)
-    {
-        if(col==mat2.row){
-            Matrix output;
-            output.initialize(row,mat2.col);
-            int r=row;
-            int c=mat2.col;
-            for (int i=0;i<r;i++){
-                for (int j=0;j<c;j++){
-                    float result=0.0f;
-                    for (int k=0;k<c;k++){
-                        result+=value_at(i,k)*mat2.value_at(k,j);
-                    }
-                    output.values[output.ind(i,j)]=result;
-                }
-            }
-            return output;
-        }
-       
-    }
     
-     Matrix operator +(Matrix mat2)
+   
+     Matrix operator +(Matrix& mat2)
     {
         if(col==mat2.col && row==mat2.row){
             Matrix output;
@@ -86,7 +69,7 @@ class Matrix{
         }
     }
     
-    Matrix operator -(Matrix mat2)
+    Matrix operator -(Matrix& mat2)
     {
         if(col==mat2.col && row==mat2.row){
             Matrix output;
@@ -102,22 +85,67 @@ class Matrix{
         }
     }
     
-    
-    Matrix multiply(float x)
+    Matrix Applyfunction(float (&func)(float))
     {
-        if(col==mat2.col && row==mat2.row){
+        
             Matrix output;
             output.initialize(row,col);
             int r=row;
             int c=col;
             for (int i=0;i<r;i++){
                 for (int j=0;j<c;j++){
-                    output.values[output.ind(i,j)]=value_at(i,j)*x
+                    output.values[output.ind(i,j)]=
+                    func(value_at(i,j));
+                }
+            }
+            return output;
+        
+    }
+    
+    
+    Matrix multiply(float x)
+    {
+            Matrix output;
+            output.initialize(row,col);
+            int r=row;
+            int c=col;
+            for (int i=0;i<r;i++){
+                for (int j=0;j<c;j++){
+                    output.values[output.ind(i,j)]=value_at(i,j)*x;
                 }
             }
             return output;
         }
+    
+    
+    
+    
+    
+     Matrix operator*(Matrix& mat2)
+    {
+            assert(col==mat2.row);
+            Matrix output;
+            output.initialize(row,mat2.col);
+            int r=row;
+            int c=mat2.col;
+            for (int i=0;i<r;i++){
+                for (int j=0;j<c;j++){
+                    float result=0.0f;
+                    for (int k=0;k<c;k++){
+                        result+=value_at(i,k)*mat2.value_at(k,j);
+                    }
+                    output.values[output.ind(i,j)]=result;
+                }
+            }
+            
+        
+        return output;
+       
     }
+    
+    
+    
+    
     
 };
 Matrix Transpose(Matrix mat)
@@ -138,18 +166,24 @@ Matrix Transpose(Matrix mat)
     
 class Layer
 {
-    virtual Matrix Forward() = 0;
-    virtual Matrix Backward() = 0;
+    public:
+    virtual Matrix Forward(Matrix m) {
+        return m;
+    }
+    virtual Matrix Backward(Matrix m) {
+        return m;
+    }
+    virtual void summary() {
+        cout<<"layer";
+    }
     
     
 };
 
-class Linear:Layer
+class Linear:public Layer
 {
     public:
-    
     Matrix weight;
-    public:
     Linear(int prev_dimension,int curr_dimension)
     {
         
@@ -159,22 +193,58 @@ class Linear:Layer
     Matrix Forward(Matrix input)
     {
         Matrix out=input*weight;
-        return out
+        
+        return out;
     }
-    Matrix Backward(Matrix input ,Matrix error)
+    Matrix Backward(Matrix input ,Matrix error,float lr)
     {
         
+        Matrix tran_wt=Transpose(weight);
+        Matrix input_error=error*tran_wt;
         
+        Matrix tran_ip=Transpose(input);
+        Matrix weight_error=tran_ip*error;
+        
+        Matrix err=weight_error.multiply(lr);
+        weight=weight-err;
+        
+        return input_error;
+        
+        
+    }
+    void summary()
+    {
+        int *x;
+        x=weight.shape();
+        cout<<"linear  in-dim  : "<<*x<<" out-dim : "<<*(x+1);
     }
 };
 class ANN
 {
     public :
-    vector<Linear> Model;
+    vector<Layer*> Model;
     vector<Matrix> inputs;//input at each layer 
+    void add(Layer  *lay){
+        Model.push_back(lay);
+    }
+    void summary(){
+        
+        for (int i=0;i<Model.size();i++){
+            Model[i]->summary();
+        }
+    }
+    
 };
 int main() {
     // Write C++ code here
+    ANN model;
+    Linear l1(5,6);
+    model.add(&l1);
+    model.summary();
+
+    
+    
+    
     /*
     Matrix test1(3,3,1);
     cout<<"\n\n";
@@ -183,7 +253,6 @@ int main() {
     Matrix out=test1*test2;
     int *x;
     x=out.shape();
-
     cout<<*x<<*(x+1);
     //cout<<*(p+1);*/
     
