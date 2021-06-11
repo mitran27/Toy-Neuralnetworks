7db1fda400f230ef7fcd95f6a04956df250448ad
Mitran R <62206653+mitran27@users.noreply.github.com>
2021-06-11 09:54:37 +0530

    Update Ann.cpp

--- Ann.cpp
+++ Ann.cpp
@@ -14,8 +14,11 @@ class Matrix{
     {   row=r;
         col=c;
         srand( (unsigned)time( NULL ) );
+        float data[]={-0.38, -0.83, -0.89, 0.45, 0.47, 0.6, -0.29, -0.68, -0.87,-0.4, -0.47, -0.1, 0.56, -0.21, -0.38, 0.06, -0.26, -0.6, -0.65, -0.46, -0.23, 0.46, 0.69, 0.61, 0.59, -0.88, 0.15, -0.15, 0.55, -0.5, 0.86, 0.6, 0.03, 0.14, -0.13, 0.44, 0.7, -0.35, -0.16, -0.94, -0.61, 0.16, -0.51, 0.85, -0.05, 0.52, -0.9, 0.72, -0.34, 0.3};
+
         for (int i =0; i < r*c; i++){
-              float r=(float)rand()/RAND_MAX;    
+              //float r=(float)rand()/RAND_MAX; 
+              float r=data[i];
               if(random)
               values.push_back(r-0.5);
               else
@@ -47,8 +50,9 @@ class Matrix{
     {
         for (int i=0;i<row;i++){
                 for (int j=0;j<col;j++){
-                    cout<<value_at(i,j)<<endl;
+                    cout<<value_at(i,j)<<"  ";
                 }
+                cout<<endl;
             }
     }
     
@@ -126,12 +130,14 @@ class Matrix{
             assert(col==mat2.row);
             Matrix output;
             output.initialize(row,mat2.col);
+           
             int r=row;
             int c=mat2.col;
             for (int i=0;i<r;i++){
                 for (int j=0;j<c;j++){
                     float result=0.0f;
-                    for (int k=0;k<c;k++){
+                    for (int k=0;k<col;k++){
+                   
                         result+=value_at(i,k)*mat2.value_at(k,j);
                     }
                     output.values[output.ind(i,j)]=result;
@@ -170,11 +176,14 @@ class Layer
     virtual Matrix Forward(Matrix m) {
         return m;
     }
-    virtual Matrix Backward(Matrix m) {
+    virtual Matrix Backward(Matrix m ,Matrix e,float lr) {
         return m;
     }
     virtual void summary() {
         cout<<"layer";
+    }
+    virtual void print_weight(){
+        cout<<"class layer doesn't hold weights";
     }
     
     
@@ -212,6 +221,9 @@ class Linear:public Layer
         
         
     }
+    void print_weight(){
+        weight.print();
+    }
     void summary()
     {
         int *x;
@@ -228,21 +240,30 @@ class ANN
         Model.push_back(lay);
     }
     Matrix cost(Matrix &prediction ,Matrix &Target){
-        Matrix err=Target-prediction
-        return err
+        Matrix err=Target-prediction;
+        return err;
     }
     
-    void epoch(Matrix Input,Matrix Output,float lr)
-    {   Matrix ip=Input
+    void epoch(Matrix Input,Matrix Target,float lr)
+    {   Matrix ip=Input;
+        cout<<Model.size()<<endl;
         for (int i=0;i<Model.size();i++){
-            inputs.push_back(ip)
-            Matrix op=Model[i]->forward(ip)
-            ip=op
+            cout<<"input for layer "<<i<<'\n';
+            ip.print();
+            inputs.push_back(ip);
+            cout<<"\nweight for layer "<<i<<'\n';
+            Model[i]->print_weight();
+            Matrix op=Model[i]->Forward(ip);
+            cout<<"output for layer "<<i<<'\n';
+            op.print();
+            cout<<"\n\n";
+            
+            ip=op;
         }
-        Matrix err=cost(ip,Output)
+        Matrix err=cost(ip,Target);
         for (int i=Model.size()-1;i<=0;i--){
-            Matrix ip_lay=inputs[i]
-            err=Model[i]->backward(ip,err,lr)
+            Matrix ip_lay=inputs[i];
+            err=Model[i]->Backward(ip,err,lr);
         }
         
     }
@@ -260,6 +281,13 @@ int main() {
     ANN model;
     Linear l1(5,6);
     model.add(&l1);
+    Linear l2(6,3);
+    model.add(&l2);
+    Matrix input;
+    Matrix output;
+    output.initialize(3,3);
+    input.initialize(3,5,1);
+    model.epoch(input,output,0.01);
     model.summary();
 
     
